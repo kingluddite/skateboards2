93f1c67f0ccebde31e79755e7ca368163d40bff9
philip howley <howley.phil@gmail.com>
2022-01-19 19:56:19 -0500

    Initialize repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus similique ipsam quo voluptatem, cumque aperiam quod quos maxime at nam officia nihil saepe laborum optio! Vitae aliquam quae hic eos!</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
